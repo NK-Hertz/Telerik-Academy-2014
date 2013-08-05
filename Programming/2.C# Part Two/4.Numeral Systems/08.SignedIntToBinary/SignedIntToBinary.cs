31dc8b2a5d73389704e5c86d665f9abcdda924b3
NK-Hertz <fester.s.son@gmail.com>
2013-08-05 16:33:45 +0300

    SignedIntToBinary.cs added

---
+++ Programming/2.C# Part Two/4.Numeral Systems/08.SignedIntToBinary/SignedIntToBinary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class SignedIntToBinary
+{
+    //Write a program that shows the binary representation of given 
+    //16-bit signed integer number (the C# type short).
+    static void Main()
+    {
+        Console.WriteLine("Enter number from -32,768 to 32,767:: ");
+        short num = short.Parse(Console.ReadLine());
+        string inBinary = Convert.ToString(num, 2).PadLeft(16, '0');
+        Console.WriteLine(inBinary);
+    }
+}

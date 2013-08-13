f6542132cdad8260b095b26dab97458bb7df8d13
NK-Hertz <fester.s.son@gmail.com>
2013-08-13 20:52:11 +0300

    6th task

--- Programming/2.C# Part Two/5.Using-Classes-and-Objects/05.NumberOfWorkDays/NumberOfWorkDays.cs
+++ Programming/2.C# Part Two/5.Using-Classes-and-Objects/05.NumberOfWorkDays/NumberOfWorkDays.cs
@@ -7,15 +7,45 @@
 
 class NumberOfWorkDays
 {
-    static DateTime[] holidays = { new DateTime(2012, 12, 24), new DateTime(2012, 12, 25), new DateTime(2012, 12, 30),
-                                   new DateTime(2012, 12, 31), new DateTime(2013, 01, 01) };
-
     static void Main()
     {
-        DateTime today = DateTime.Today;
+        Console.WriteLine("Enter end date : YYYY/MM/DD : ");
+        string[] endDayArr = Console.ReadLine().Split('/');
+        int year = int.Parse(endDayArr[0]);
+        int month = int.Parse(endDayArr[1]);
+        int day = int.Parse(endDayArr[2]);
 
-        //today and a given day
-        //workdays - monday - friday
-        //fixed list of holidays - as array
+        DateTime today = DateTime.Today;
+        DateTime selectedDay = new DateTime(year, month, day);
+        int daysLenght = (selectedDay - today).Days;
+
+        DateTime[] Holidays = { new DateTime(2013, 12, 24), new DateTime(2013, 12, 25), new DateTime(2013, 12, 30),
+                                   new DateTime(2013, 12, 31), new DateTime(2014, 01, 01), new DateTime(2014, 04, 02) };
+
+        int workDays = 0;
+        bool isHoliday = false;
+
+        for (int i = 0; i < daysLenght; i++)
+        {
+            today = today.AddDays(1);
+            if (today.DayOfWeek != DayOfWeek.Saturday && today.DayOfWeek != DayOfWeek.Sunday)
+            {
+                for (int j = 0; j < Holidays.Length; j++)
+                {
+                    if (today == Holidays[j])
+                    {
+                        isHoliday = true;
+                        break;
+                    }
+                }
+
+                if (!isHoliday)
+                {
+                    workDays++;
+                }
+
+            }
+        }
+        Console.WriteLine(workDays);
     }
 }
---
+++ Programming/2.C# Part Two/5.Using-Classes-and-Objects/06.SumSequenceInStr/SumSequenceInStr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*You are given a sequence of positive integer values written into a string, 
+ * separated by spaces. Write a function that reads these values from given
+ * string and calculates their sum. 
+ * Example:string = "43 68 9 23 318"  result = 461
+*/
+class SumSequenceInStr
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter sequence: ");
+        string[] sequenceArr = Console.ReadLine().Split(' ');
+        int[] sequenceInt = new int[sequenceArr.Length];
+        for (int i = 0; i < sequenceArr.Length; i++)
+        {
+            sequenceInt[i] = int.Parse(sequenceArr[i]);
+        }
+
+        int result = 0;
+        for (int i = 0; i < sequenceArr.Length; i++)
+        {
+            result +=sequenceInt[i];
+        }
+        Console.WriteLine(result);
+    }
+}

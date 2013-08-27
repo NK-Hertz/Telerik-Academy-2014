2ed08384eb3789603df7d93116a48d8250da4609
NK-Hertz <fester.s.son@gmail.com>
2013-08-27 16:51:37 +0300

    Text Files Added Two tasks

---
+++ Programming/2.C# Part Two/7.Text-Files/01.PrintOddLines/PrintOddLines.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+//Write a program that reads a text file and prints on the console its odd lines.
+
+class PrintOddLines
+{
+    static void Main()
+    {
+        
+        StreamReader reader = new StreamReader(@"..\..\PrintOddLines.cs");
+        using (reader)
+        {
+            int lineNumb = 1;
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                
+                if (lineNumb % 2 != 0)
+                {
+                    Console.WriteLine(line);
+                }
+                lineNumb++;
+                line = reader.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming/2.C# Part Two/7.Text-Files/02.ConcatenateTxtFilesInNew/NewTxtFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+//Write a program that concatenates two text files into another text file.
+
+class NewTxtFile
+{
+    static void Main()
+    {
+        string newTxtFile = @"..\..\NewTxtFile.cs";
+        string appConfig = @"..\..\App.config";
+        StreamWriter writer = new StreamWriter(@"..\..\result.txt");
+        using (writer)
+        {
+            using (StreamReader reader = new StreamReader(newTxtFile))
+            {
+                string line = reader.ReadToEnd();
+                writer.WriteLine(line);
+            }
+
+            using (StreamReader reader = new StreamReader(appConfig))
+            {
+                string line = reader.ReadToEnd();
+                writer.WriteLine(line);
+            }
+
+            Console.WriteLine("Concatenation Ready!");
+        }
+    }
+}

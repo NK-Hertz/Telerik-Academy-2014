bc659b19586377a03de248f43925eeda1c229ff7
NK-Hertz <fester.s.son@gmail.com>
2013-08-27 23:55:55 +0300

    tasks added

---
+++ Programming/2.C# Part Two/7.Text-Files/12.RemoveWordsFromList/RemoveWordsFromList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Security;
+
+/* Write a program that removes from a text file all words listed in given another 
+ * text file. Handle all possible exceptions in your methods.
+ */
+class RemoveWordsFromList
+{
+    static void Main()
+    {
+        try
+        {
+            string regex = @"\b(" + String.Join("|", File.ReadAllLines("../../words.txt")) + @")\b";
+
+            using (StreamReader reader = new StreamReader(@"..\..\text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"..\..\result.txt"))
+                {
+                    for (string line; (line = reader.ReadLine()) != null; )
+                    {
+                        writer.WriteLine(Regex.Replace(line, regex, ""));
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (SecurityException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Programming/2.C# Part Two/7.Text-Files/13.WordCounterFromFiles/WordCounterFromFiles.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Security;
+
+/*Write a program that reads a list of words from a file words.txt and finds how many 
+ * times each of the words is contained in another file test.txt. The result should be
+ * written in the file result.txt and the words should be sorted by the number of 
+ * their occurrences in descending order. Handle all possible exceptions in your 
+ * methods.
+ */
+class WordCounterFromFiles
+{
+    static void Main()
+    {
+        try
+        {
+            string[] words = File.ReadAllLines("../../words.txt");
+            int[] count = new int[words.Length];
+
+            using (StreamReader reader = new StreamReader("../../text.txt"))
+            {
+                for (string line; (line = reader.ReadLine()) != null; )
+                {
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        count[i] += Regex.Matches(line, @"\b" + words[i] + @"\b").Count;
+                    }
+                }
+            }
+
+            Array.Sort(count, words);
+
+            using (StreamWriter writer = new StreamWriter("../../result.txt"))
+            {
+                for (int i = words.Length - 1; i >= 0; i--)
+                {
+                    writer.WriteLine("{0} : {1} times", words[i], count[i]);
+                }
+            }
+            Console.WriteLine("Finished succesfully!");
+        }
+
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (SecurityException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}

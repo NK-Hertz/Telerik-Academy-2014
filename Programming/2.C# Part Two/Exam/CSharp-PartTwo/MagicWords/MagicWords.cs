f8765ad57b5aa6789bdec28c3f20f10e2013b8a2
NK-Hertz <fester.s.son@gmail.com>
2014-12-18 14:06:56 +0200

    Exam, Presentations Added // Task descriptions are in the last pages of the presentations

---
+++ Programming/2.C# Part Two/Exam/CSharp-PartTwo/MagicWords/MagicWords.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class MagicWords
+{
+    static void Main()
+    {
+        int numberOfWords = int.Parse(Console.ReadLine());
+        //List<string> magicWords = new List<string>();
+
+        //for (int i = 0; i < numberOfWords; i++)
+        //{
+        //    magicWords.Add(Console.ReadLine());
+        //}
+
+        int wordLenght = 0;
+        int postion = 0;
+        int index = 0;
+        string item = "";
+
+        string[] magicWords = new string[numberOfWords];
+        string[] newMagicWords = new string[numberOfWords];
+        for (int i = 0; i < numberOfWords; i++)
+        {
+            magicWords[i] = Console.ReadLine();
+        }
+
+        for (int i = 0; i < numberOfWords; i++)
+        {
+            wordLenght = magicWords[index].Length;
+            postion = wordLenght % (numberOfWords + 1);
+            if (postion >= numberOfWords)
+            {
+                postion = postion % numberOfWords;
+                postion++;
+            }
+
+            if (newMagicWords[postion] != null)
+            {
+                newMagicWords[index] = newMagicWords[postion];
+            }
+
+            newMagicWords[postion] = magicWords[index];
+
+            
+
+            index++;
+        }
+
+        //reorder
+        //for (int i = 0; i < numberOfWords - 1; i++)
+        //{
+        //    wordLenght = magicWords[index].Length;
+        //    postion = wordLenght % (numberOfWords + 1);
+        //    item = magicWords[index];
+        //    if (postion >= numberOfWords)
+        //    {
+        //        postion = postion % numberOfWords;
+        //        postion++;
+        //    }
+        //    else if (postion <= 0)
+        //    {
+        //        postion = 1;
+        //    }
+        //    magicWords.RemoveAt(index);
+        //    magicWords.Insert(postion-1, item);
+        //    index = postion - 1;
+        //    index++;
+        //}
+
+        //print
+        StringBuilder str = new StringBuilder();
+        string bucket;
+        index = 0;
+        int charIndex = 0;
+        int bestCharCount = 0;
+
+        while (true)
+        {
+            bucket = newMagicWords[index];
+            int charCount = newMagicWords[index].Length;
+
+            if (charCount > bestCharCount)
+            {
+                bestCharCount = charCount;
+            }
+
+            if (charIndex == bestCharCount)
+            {
+                break;
+            }
+
+            if (index == numberOfWords)
+            {
+                index = 0;
+                charIndex++;
+            }
+
+            if (charIndex >= newMagicWords[index].Length)
+            {
+                index++;
+                if (index == numberOfWords)
+                {
+                    index = 0;
+                    charIndex++;
+                }
+                continue;
+            } 
+            
+            
+            str.Append(bucket[charIndex]);
+            index++;
+
+            if (index == numberOfWords)
+            {
+                index = 0;
+                charIndex++;
+            }
+
+            if (charIndex > newMagicWords[index].Length)
+            {
+                continue;
+            }
+        }
+
+        Console.WriteLine(str);
+            
+    }
+}
---
+++ Programming/2.C# Part Two/Exam/CSharp-PartTwo/MultiverseCommunication/MultiverseCommunication.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class MultiverseCommunication
+{
+    static int count = 0;
+
+    static void Main()
+    {
+        List<string> pizzaLang = new List<string>();
+        InitiateList(pizzaLang);
+
+        string input = Console.ReadLine().ToUpper();
+
+        List<string> inputSeparated = new List<string> ();
+        int lenghtOfWord = 3;
+
+        for (int i = 0; i < input.Length; i+=lenghtOfWord)
+        {
+            inputSeparated.Add(input.Substring(i,lenghtOfWord));
+        }
+
+        List<int> output = new List<int>();
+        
+        for (int i = 0; i < inputSeparated.Count; i++)
+        {
+            for (int j = 0; j < pizzaLang.Count; j++)
+			{
+                if (inputSeparated[i] == pizzaLang[j])
+                {
+                    output.Add(j) ;
+                    count++;
+                }
+                
+			}
+        }
+
+        ulong result = 0;
+        int thirteenth = 1;
+        for (int o = 0; o < output.Count; o++)
+        {
+            thirteenth = Thirteenth(count);
+            result +=  (ulong)(thirteenth*output[o]);
+            count--;
+            thirteenth = 0;
+        }
+
+        Console.WriteLine(result);
+
+    }
+
+    private static int Thirteenth(int count)
+    {
+        int thirtheenth = 1;
+        for (int i = 0; i < count - 1; i++)
+        {
+            thirtheenth = thirtheenth * 13;
+        }
+
+        return thirtheenth;
+    }
+
+    private static void InitiateList(List<string> pizzaLang)
+    {
+        pizzaLang.Add("CHU");
+        pizzaLang.Add("TEL");
+        pizzaLang.Add("OFT");
+        pizzaLang.Add("IVA");
+        pizzaLang.Add("EMY");
+        pizzaLang.Add("VNB");
+        pizzaLang.Add("POQ");
+        pizzaLang.Add("ERI");
+        pizzaLang.Add("CAD");
+        pizzaLang.Add("K-A");
+        pizzaLang.Add("IIA");
+        pizzaLang.Add("YLO");
+        pizzaLang.Add("PLA");
+    }
+}
